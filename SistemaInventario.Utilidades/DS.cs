47f81d784d9d1a65f6dc11b713c3ffc79993bed9
IvanCarrieri <ivancarrieri@yahoo.com.ar>
2024-02-23 16:03:48 -0300

    Pagos

---
+++ SistemaInventario.Utilidades/DS.cs
@@ -0,0 +1,39 @@
+﻿namespace SistemaInventario.Utilidades
+{
+    public class DS
+    {
+        public const string Exitosa = "Exitosa";
+        public const string Error = "Error";
+
+        public const string ImagenRuta = @"\Imagenes\Producto\";
+
+
+        public const string RoleAdmin = "Admin";
+        public const string RoleCliente = "Cliente";
+        public const string RoleInventario = "Inventario";
+
+        public const string SesionCarrito = "Sesion carrito";
+
+        //Estados de la orden
+
+        public const string EstadoPendiente = "Pendiente";
+        public const string EstadoAprobado = "Aprobado";
+        public const string EstadoEnProceso = "En proceso";
+        public const string EstadoEnviado = "Enviado";
+        public const string EstadoCancelado = "Cancelado";
+        public const string EstadoDevuelto = "Devuelto";
+
+        //estado del pago de la orden
+        public const string PagoPendiente = "Pendiente";
+        public const string PagAprobado = "Aprobado";
+        public const string PagoRetrasado = "Retrasado";
+        public const string PagoRechazado = "Rechazado";
+
+
+
+
+
+
+
+    }
+}
